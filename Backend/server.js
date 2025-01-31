40ca8484e04ec4afbfcd153ad51c8ff45ad8b073
suheila-77 <zuheilaupdalla7@gmail.com>
2025-01-31 18:02:56 +0300

    updated the server

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,10 @@ const mongoose= require("mongoose")
 const cors = require("cors")
 const { config } = require('dotenv');
 const app = express()
-
+const adminRoutes = require("./router/adminRouter");
+const userRoutes = require("./router/userRoute")
+const complaintRoutes = require("./routes/complaintRoutes");
+app.use("/api", complaintRoutes);
 // Load environment variables
 config();
 
@@ -14,7 +17,8 @@ connectDB();
 app.use(express.json())
 
 app.use(cors())
-
+app.use(adminRouter)
+app.use(complaintRouter)
 
   mongoose.connect("mongodb://localhost:27017/OnlineComplaint").then(()=>{
     console.log("the database is connected successfuly")
@@ -24,7 +28,9 @@ app.use(cors())
 })
 
 
-
+app.use("/api", adminRoutes);
+app.use("/api", userRoutes )
+app.use("/api", complaintRoutes)
  app.listen  (5000,   ()=>{
     console.log("the server port is running on port 5000")
 })
