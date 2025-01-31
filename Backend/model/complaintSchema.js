4772159926052055848aafd30d3fe5c57a64e320
suheila-77 <zuheilaupdalla7@gmail.com>
2025-01-31 17:46:09 +0300

    complaint schema

---
+++ Backend/model/complaintSchema.js
@@ -0,0 +1,20 @@
+
+const mongoose  = require("mongoose")
+
+const complaintSchema = mongoose.Schema({
+
+
+    Title: {
+        type: String,
+        required: true,
+    },
+
+
+    Description: {
+        type: String,
+        required: true,
+    }
+
+})
+
+module.exports  = mongoose.model("complains", complaintSchema)
