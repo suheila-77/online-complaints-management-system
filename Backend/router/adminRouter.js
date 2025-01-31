919a86067416800a2e9d8d6695dedc3f63b95988
suheila-77 <zuheilaupdalla7@gmail.com>
2025-01-31 17:58:29 +0300

    admin router

---
+++ Backend/router/adminRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { registerAdmin, loginAdmin, adminProtectedRoute } = require("../controller/adminController");
+const verifyAdmin = require("../middlewares/adminAuth"); // Import middleware
+
+const router = express.Router();
+
+router.post("/admin/create", registerAdmin);
+router.post("/admin/login", loginAdmin);
+router.get("/admin/protected", verifyAdmin, adminProtectedRoute);
+
+module.exports = router;
