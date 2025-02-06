2ddc574934aeaeee7ae2583bee654dea583ca16e
suheila-77 <zuheilaupdalla7@gmail.com>
2025-02-06 08:16:53 +0300

    designed the view page

--- Backend/routes/complaintRouter.js
+++ Backend/routes/complaintRouter.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const { createComplaint, getAllComplaints, updateComplaint, deleteComplaint } = require("../controller/complaintController");
+const { createComplaint, getAllComplaints,  deleteComplaint } = require("../controller/complaintController");
 
 const router = express.Router();
 
 router.post("/complaint", createComplaint);
 router.get("/complaint/management", getAllComplaints);
-router.put("/complaint/update/:id", updateComplaint);
 router.delete("/complaint/delete/:id", deleteComplaint);
 
 module.exports = router;
